d792c7946fefe6754c02495ba9dadb3b9a525293
dlc05 <spdanny97@gmail.com>
2022-11-17 18:38:54 +0100

    Parte 322

---
+++ Dvd.php
@@ -0,0 +1,21 @@
+<?php
+class Dvd extends Soporte{
+
+    public string $idiomas;
+    public string $formatoPantalla;
+
+    public function __construct($titulo, $numero, $precio, $idiomas, $formatoPantalla)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->idiomas = $idiomas;
+        $this->formatoPantalla = $formatoPantalla;
+    }
+
+    public function muestraResumen() : void{
+        echo "<br>Película en DVD: ";
+        echo "<br><i>$this->titulo</i>";
+        echo "<br>".$this->getPrecio()."€ (IVA no incluido)";
+        echo "<br>Idiomas: ".$this->idiomas;
+        echo "<br>Formato Pantalla: ".$this->formatoPantalla;
+    }
+}
--- Inicio.php
+++ Inicio.php
@@ -18,3 +18,13 @@
     echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
     echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
     $miCinta->muestraResumen();
+
+    echo "<br><br>------------------------<br><br>";
+    //Parte 322
+    include "Dvd.php";
+
+    $miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+    echo "<strong>" . $miDvd->titulo . "</strong>";
+    echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
+    echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
+    $miDvd->muestraResumen();

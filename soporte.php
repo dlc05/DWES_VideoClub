78f0f0311e8ed055b855b1887d479a57761e3da6
dlc05 <spdanny97@gmail.com>
2022-11-17 18:02:12 +0100

    Parte 320

--- soporte.php
+++ soporte.php
@@ -1,1 +1,36 @@
 <?php
+class Soporte{
+
+    public string $titulo;
+    protected int $numero;
+    private float $precio;
+    const IVA = 1.21;
+
+    public function __construct($titulo, $numero, $precio)
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
+
+    public function getPrecio(): float
+    {
+        return $this->precio;
+    }
+
+    public function getPrecioConIVA(): float
+    {
+        return $this->precio*Soporte::IVA;
+    }
+
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    public function muestraResumen() : void{
+        echo "<br><i>$this->titulo</i>";
+        echo "<br>$this->precio € (IVA no incluido)";
+    }
+}
+

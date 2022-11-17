aec52d8fed30e89571312cd74237b1265a5c9114
dlc05 <spdanny97@gmail.com>
2022-11-17 18:54:54 +0100

    Parte 323

--- Inicio.php
+++ Inicio.php
@@ -28,3 +28,13 @@
     echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
     echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
     $miDvd->muestraResumen();
+
+    echo "<br><br>------------------------<br><br>";
+    //Parte 323
+    include "Juego.php";
+
+    $miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);
+    echo "<strong>" . $miJuego->titulo . "</strong>";
+    echo "<br>Precio: " . $miJuego->getPrecio() . " euros";
+    echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+    $miJuego->muestraResumen();
---
+++ Juego.php
@@ -0,0 +1,36 @@
+<?php
+class Juego extends Soporte{
+
+    public string $consola;
+    private int $minNumJugadores;
+    private int $maxNumJugadores;
+
+    public function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
+    }
+
+    public function muestraJugadoresPosible() : string{
+        if($this->minNumJugadores == 1 && $this->maxNumJugadores == 1){
+            return "Para un jugador";
+        }
+        if($this->minNumJugadores == 1){
+            return "Para ".$this->maxNumJugadores." jugadores";
+        }
+        if($this->minNumJugadores > 1){
+            return "De ".$this->minNumJugadores. " a " .$this->maxNumJugadores. " jugadores";
+        }
+
+        return "Error en la muestra de jugadores";
+    }
+
+    public function muestraResumen() : void{
+        echo "<br>Juego para: ".$this->consola;
+        echo "<br><i>$this->titulo</i>";
+        echo "<br>".$this->getPrecio()."€ (IVA no incluido)";
+        echo "<br>".$this->muestraJugadoresPosible();
+    }
+}
